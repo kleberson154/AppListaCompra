358974ab5cf4ae0e0fcdacdf21574643c79fa230
kleberson154 <kleberson55@hotmail.com>
2025-06-23 14:19:40 -0300

    Deixando a formatacao do prexo explicita

--- app/src/main/java/com/kleberson/listacompra/util/ViewHolder.kt
+++ app/src/main/java/com/kleberson/listacompra/util/ViewHolder.kt
@@ -5,11 +5,12 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.kleberson.listacompra.R
 import com.kleberson.listacompra.model.Food
+import java.util.Locale
 
 class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(food: Food) {
         itemView.findViewById<TextView>(R.id.nomeTextView).text = food.name
-        itemView.findViewById<TextView>(R.id.precoTextView).text = food.price.toString()
+        itemView.findViewById<TextView>(R.id.precoTextView).text = String.format(Locale.US, "%.2f", food.price)
     }
 
 }
